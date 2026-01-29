5b704fcd3da054dfa392eec16c75d82d01024285
Aditya Magar <magaraditya2911@gmail.com>
2026-01-29 13:21:24 +0530

    Ui componnents added

---
+++ taskmaster/src/Routes.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { BrowserRouter, Routes as RouterRoutes, Route } from "react-router-dom";
+import ScrollToTop from "components/ScrollToTop";
+import ErrorBoundary from "components/ErrorBoundary";
+import NotFound from "pages/NotFound";
+import UserLogin from './pages/user-login';
+import LandingPage from './pages/landing-page';
+import Dashboard from './pages/dashboard';
+import UserRegistration from './pages/user-registration';
+import ProgressAnalytics from './pages/progress-analytics';
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <ErrorBoundary>
+      <ScrollToTop />
+      <RouterRoutes>
+        {/* Define your route here */}
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/user-login" element={<UserLogin />} />
+        <Route path="/landing-page" element={<LandingPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/user-registration" element={<UserRegistration />} />
+        <Route path="/progress-analytics" element={<ProgressAnalytics />} />
+        <Route path="*" element={<NotFound />} />
+      </RouterRoutes>
+      </ErrorBoundary>
+    </BrowserRouter>
+  );
+};
+
+export default Routes;
